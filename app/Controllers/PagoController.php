abd99fe84165e0b372ba0a1df22d4a3f77ff6277
Pablo Orejuela <pabloorejuela@hotmail.com>
2022-07-11 20:43:19 -0500

    Creados los controladores para cada tabla de la DB

---
+++ app/Controllers/PagoController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+
+class PagoController extends BaseController
+{
+    public function index()
+    {
+        //
+    }
+}
