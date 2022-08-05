31a44cb714891274993567381f783bb69ce0036a
Pablo Orejuela <pabloorejuela@hotmail.com>
2022-08-05 09:19:51 -0500

    Creada la vista reporte cobros cooperativas

--- app/Views/reportes/frm_cooperativas_reporte_pagos.php
+++ app/Views/reportes/frm_cooperativas_reporte_pagos.php
@@ -12,8 +12,22 @@
                     <form action="<?php echo base_url().'/getExcel';?>" method="post" id="form-subir-excel" enctype="multipart/form-data">
                         <?= csrf_field(); ?>
                         <div class="container mb-5" style="margin-top:30px;">
+                            <div class="mb-3 row">
+                                <label for="date_desde" class="col-sm-2 col-form-label">Desde: </label>
+                                <div class="col-md-4">
+                                    <input type="date" class="form-control" name="date_desde" id="date" value="<?= date('Y-m-d');?>">
+                                    <p id="error-message"><?= session('errors.date_desde');?> </p>
+                                </div>
+                            </div>
+                            <div class="mb-3 row">
+                                <label for="date_hasta" class="col-sm-2 col-form-label">Hasta: </label>
+                                <div class="col-md-4">
+                                    <input type="date" class="form-control" name="date_hasta" id="date" value="<?= date('Y-m-d');?>">
+                                    <p id="error-message"><?= session('errors.date_hasta');?> </p>
+                                </div>
+                            </div>
                             <div class="col-sm-6 mb-3">
-                                <label for="idrol" class="col-sm-2 col-form-label">Cooperativa: </label>
+                                <label for="idrol" class="col-sm-4 col-form-label">Cooperativa: </label>
                                 <select class="form-select" aria-label="Default select example" value="<?= old('idempresa'); ?>" name="idempresa">
                                     <option value="0" selected>-- Escoja una cooperativa --</option>
                                     <?php  
